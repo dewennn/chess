9cd391d23052f86968cb2948bc46040b9b296b97
dewen <derren9283@gmail.com>
2023-08-21 01:02:39 +0700

    base setup update

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,6 +1,6 @@
 import { Pawn, Horse, Bishop, Rook, Queen, King } from './chessPieces.js'
 
-const setChessboardBase = () => {
+export const setChessboardBase = () => {
     let html = '';
     let ctr = 0;
 
@@ -20,36 +20,16 @@ const setChessboardBase = () => {
     document.querySelector('.chessboard').innerHTML = html;
 }
 
-export const setupChessboard = (chessPieces) => {
-    // Pawns
-    for(let i = 0; i < 8; i++){
-        chessPieces.push(new Pawn('black', [1, i]));
-        chessPieces.push(new Pawn('white', [6, i]));
-    }
+export const setupChessboard = () => {
 
-    // Horses
-    chessPieces.push(new Horse('black', [0, 1]));
-    chessPieces.push(new Horse('black', [0, 6]));
-    chessPieces.push(new Horse('white', [7, 1]));
-    chessPieces.push(new Horse('white', [7, 6]));
-
-    // Bishops
-    chessPieces.push(new Bishop('black', [0, 2]));
-    chessPieces.push(new Bishop('black', [0, 5]));
-    chessPieces.push(new Bishop('white', [7, 2]));
-    chessPieces.push(new Bishop('white', [7, 5]));
-
-    // Rooks
-    chessPieces.push(new Rook('black', [0, 0]));
-    chessPieces.push(new Rook('black', [0, 7]));
-    chessPieces.push(new Rook('white', [7, 0]));
-    chessPieces.push(new Rook('white', [7, 7]));
-
-    // Kings and Queens
-    chessPieces.push(new Queen('black', [0, 3]));
-    chessPieces.push(new Queen('white', [7, 3]));
-    chessPieces.push(new King('black', [0, 4]));
-    chessPieces.push(new King('white', [7, 4]));
-
-    setChessboardBase();
+    return [
+        [new Rook('black', [0, 0]), new Horse('black', [0, 1]), new Bishop('black', [0, 2]), new Queen('black', [0, 3]), new King('black', [0, 4]), new Bishop('black', [0, 5]), new Horse('black', [0, 6]), new Rook('black', [0, 7])],
+        [new Pawn('black', [1, 0]), new Pawn('black', [1, 1]), new Pawn('black', [1, 2]), new Pawn('black', [1, 3]), new Pawn('black', [1, 4]), new Pawn('black', [1, 5]), new Pawn('black', [1, 6]), new Pawn('black', [1, 7])],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [new Pawn('white', [6, 0]), new Pawn('white', [6, 1]), new Pawn('white', [6, 2]), new Pawn('white', [6, 3]), new Pawn('white', [6, 4]), new Pawn('white', [6, 5]), new Pawn('white', [6, 6]), new Pawn('white', [6, 7])],
+        [new Rook('white', [7, 0]), new Horse('white', [7, 1]), new Bishop('white', [7, 2]), new Queen('white', [7, 3]), new King('white', [7, 4]), new Bishop('white', [7, 5]), new Horse('white', [7, 6]), new Rook('white', [7, 7])]
+    ];
 }
